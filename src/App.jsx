8f46cb5360a003c579284341425f5b3c05d05e5c
Parth Panara <parthpanara91@gmail.com>
2026-04-04 03:39:06 +0530

    Add comment to improve code readability

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { InputBox } from './components'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
 
+// React - Currency Converter
 function App() {
   const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState('usd');
